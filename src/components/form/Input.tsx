00e51a89c10612bdd1ff0f3decde5100a90224a7
mdmmalamin <mdmmalamin@gmail.com>
2024-09-23 05:09:31 +0600

    add Login page

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type TInputProps = {
+  className?: string;
+  name?: string;
+  id?: string;
+  type?: string;
+  // ...props?: any;
+  register?: any;
+};
+
+const Input = ({
+  className = "",
+  name = "name",
+  id = "id",
+  type = "text",
+  register,
+  ...props
+}: TInputProps) => {
+  console.log(props);
+  return (
+    <input
+      className={`${className} mt-1 p-2 w-full border rounded-md`}
+      name={name}
+      id={id}
+      type={type}
+      {...register}
+      {...props}
+    />
+  );
+};
+
+export default Input;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,63 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import Input from "../components/form/Input";
+import Container from "../components/ui/Container";
+import Title from "../components/ui/Title";
+import Button from "../components/ui/Button";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
+  // const defaultValues = {
+  //   admin: {
+  //     email: "test@fs.com",
+  //     password: "12345678",
+  //   },
+  // };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Container>
+      <div className="max-w-md mx-auto relative overflow-hidden z-10 bg-white p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-secondary before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-primary after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
+        <Title>Please Login</Title>
+
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-600"
+              htmlFor="email"
+            >
+              Email Address
+            </label>
+            <Input id="email" type="email" register={register("email")} />
+          </div>
+
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-600"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <Input
+              id="password"
+              type="password"
+              register={register("password")}
+            />
+          </div>
+
+          <div className="flex justify-end">
+            <Button className="px-8">Log In</Button>
+          </div>
+        </form>
+      </div>
+    </Container>
   );
 };
 
